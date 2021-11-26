5c86a937ebfa293b3715a801930583c7db26eeb2
liuyanxing <liuyanxing@jwzg.com>
2021-11-26 14:26:41 +0800

    fix

--- TypeScript/typeChallenge.ts
+++ TypeScript/typeChallenge.ts
@@ -24,3 +24,34 @@ type C = Exclude<ABC, AB>;
 type Subset<T, U> = (U extends T ? true : false) extends true ? true : false;
 type S = Subset<ABC, AB>; 
 type NS = Subset<ABC, BCD>; 
+
+
+type MyReadOnly2<T, U extends keyof T> = T & { readonly [p in U]: T[p] };
+
+type DeepOnly<T> = { readonly [P in keyof T]: T[P] extends {} ? DeepOnly<T[P]> : T[P]}
+
+type TupleToUnion<T> = T extends (infer U)[] ? U : never;
+
+type LastElement<T extends any[]> = T extends [...unknown[], infer U] ? U : never; 
+
+type Pop<T extends any[]> = T extends [...infer K, infer U] ? K : never;
+
+declare function PromiseAll<T extends unknown[]>(params: T): {
+  [Index in keyof T]: T[Index] extends Promise<infer U> ? U : T[Index];
+}
+PromiseAll([1, Promise.resolve(2)]);
+
+interface Cat {
+  type: 'cat'
+  breeds: 'Abyssinian' | 'Shorthair' | 'Curl' | 'Bengal'
+}
+
+interface Dog {
+  type: 'dog'
+  breeds: 'Hound' | 'Brittany' | 'Bulldog' | 'Boxer'
+  color: 'brown' | 'white' | 'black'
+}
+
+type LookUp<T, K> = T extends { 'type': K } ? T : never;
+
+type PickByType<T, K> = { [ p in keyof T as K extends T[p] ? p : never]: T[p] };
